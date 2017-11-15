2669e82e50b7ee63995d8a12d952a544262f5b13
Guest <guest@guests-mbp>
2017-11-15 17:26:40 -0600

    Initial Commit

---
+++ Target Support Files/Pods-QuikFix/Pods-QuikFix-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QuikFix : NSObject
+@end
+@implementation PodsDummy_Pods_QuikFix
+@end
---
+++ Target Support Files/Pods-QuikFix/Pods-QuikFix-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_QuikFixVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_QuikFixVersionString[];
+
